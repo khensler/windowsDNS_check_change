d3b496f102664a7671f021d94289aeba27283f0f
khensler <46349570+khensler@users.noreply.github.com>
2020-02-03 16:24:57 -0500

    Update linux.bat

--- linux.bat
+++ linux.bat
@@ -1,6 +1,7 @@
 REM Install putty with pscp.  change to that dir (c:\program files\putty)
 REM make a list of all the server ips in c:\temp\list.txt
 REM make a resolv.conf file in c:\temp (make sure its unix formated)
+REM make sure to set the right user and pass
 REM run this from the command prompt.  not powershell.  do not run this as a bat file.  copy and paste it.  
 REM if you want to run it as a bat file REM  the first line and un REM the next (i think...)
 
