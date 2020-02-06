b9abfc54984b1c6e7ee1409031b0dee3ee4fc566
khensler <46349570+khensler@users.noreply.github.com>
2020-02-06 16:04:03 -0500

    Update linux.bat

--- linux.bat
+++ linux.bat
@@ -6,4 +6,6 @@ REM run this from the command prompt.  not powershell.  do not run this as a bat
 REM if you want to run it as a bat file REM  the first line and un REM the next (i think...)
 
 FOR /F %G IN (c:\temp\list.txt) DO echo Y | pscp -l root -pw passw0rd c:\temp\resolv.conf %G:/etc/resolv.conf
+FOR /F %G IN (c:\temp\list.txt) DO echo Y | plink %G -l root -pw passw0rd find /etc/sysconfig/network-scripts/ -name "ifcfg-*" -exec sed -i.bak -r 's/DNS1=[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/DNS1=1.2.3.4/' {} \;
+FOR /F %G IN (c:\temp\list.txt) DO echo Y | plink %G -l root -pw passw0rd find /etc/sysconfig/network-scripts/ -name "ifcfg-*" -exec sed -i.bak -r 's/DNS2=[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/DNS2=1.2.3.4/' {} \;
 REM FOR /F %%G IN (c:\temp\list.txt) DO echo Y | pscp -l root -pw passw0rd c:\temp\resolv.conf %%G:/etc/resolv.conf
