224b7f9924a80b908a43a4bcc910c6bd365aa366
khensler <46349570+khensler@users.noreply.github.com>
2020-02-06 16:04:49 -0500

    Update linux.bat

--- linux.bat
+++ linux.bat
@@ -3,6 +3,7 @@ REM make a list of all the server ips in c:\temp\list.txt
 REM make a resolv.conf file in c:\temp (make sure its unix formated)
 REM make sure to set the right user and pass
 REM run this from the command prompt.  not powershell.  do not run this as a bat file.  copy and paste it.  
+REM make sure to set the DNS1 and DNS2 entries correctly.
 REM if you want to run it as a bat file REM  the first line and un REM the next (i think...)
 
 FOR /F %G IN (c:\temp\list.txt) DO echo Y | pscp -l root -pw passw0rd c:\temp\resolv.conf %G:/etc/resolv.conf
